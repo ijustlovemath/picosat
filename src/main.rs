5d69053b2c1756de07b2c5955bff48a2b8969dfb
Jeremy <jcdejournett@gmail.com>
2020-10-15 13:00:32 -0400

    refactoring the code to be a little bit cleaner, but is it rusty?

--- src/main.rs
+++ src/main.rs
@@ -48,28 +48,11 @@ fn line_to_buffer(mode: OperatingMode, line: String)
 #[derive(Debug)]
 struct ExncOptions {
     mode: OperatingMode,
-    dest: SocketAddr
+    dest: SocketAddr,
+    sock: UdpSocket
 }
 
-fn get_cli_options() -> ExncOptions {
-    let mut ascii_hex = false;
-    let mut binary = true;
-    let mut dest = "127.0.0.1:6868".to_string();
-    {
-        let mut parser = ArgumentParser::new();
-        parser.set_description("Echo binary data to a UDP socket");
-        parser.refer(&mut ascii_hex)
-            .add_option(&["-a", "--hex"], StoreTrue,
-                        "Interpret input data as ASCII formatted hexadecimal bytes, where each newline creates a new message");
-        parser.refer(&mut binary)
-            .add_option(&["-b", "--binary"], StoreTrue,
-                        "Interpret input data as raw binary, forwarded directly to the socket");
-        parser.refer(&mut dest)
-            .add_option(&["-d", "--destination"], Store,
-                        "Destination address for the UDP data (formatted host_ip:port_number)");
-        parser.parse_args_or_exit();
-    }
-
+fn check_mode(ascii_hex: bool, binary: bool) -> OperatingMode {
     // Check that the options are valid
     if (ascii_hex && binary) {
         // we can only have one of these
@@ -82,12 +65,58 @@ fn get_cli_options() -> ExncOptions {
     } else {
         mode = OperatingMode::InputIsBinary;
     }
+    return mode;
+}
 
+fn check_dest_address(dest: String) -> SocketAddr {
+    // Check the destination address
     let destination: SocketAddr = dest.parse().expect("invalid destination address specified, make sure it follows host_ip:port syntax");
+    return destination;
+}
+
+fn check_source_socket(port: String) -> UdpSocket {
+    // check the source port
+    let port: u16 = port.parse().expect("invalid port specified, must be an unsigned 16-bit integer (0-65535)");
+    let addrs = [
+        SocketAddr::from(([127,0,0,1], port)),
+    ];
+    let sock = UdpSocket::bind(&addrs[..]).expect("unable to bind to UDP socket for sending data");
+    return sock;
+}
+
+fn get_cli_options() -> ExncOptions {
+    let mut ascii_hex = false;
+    let mut binary = false;
+    let mut dest = "127.0.0.1:6868".to_string();
+    let mut port = "45354".to_string();
+    {
+        let mut parser = ArgumentParser::new();
+        parser.set_description("Echo binary data to a UDP socket");
+        parser.refer(&mut ascii_hex)
+            .add_option(&["-a", "--hex"], StoreTrue,
+                        "Interpret input data as ASCII formatted hexadecimal bytes, where each newline creates a new message");
+        parser.refer(&mut binary)
+            .add_option(&["-b", "--binary"], StoreTrue,
+                        "Interpret input data as raw binary, forwarded directly to the destination");
+        parser.refer(&mut dest)
+            .add_option(&["-d", "--destination"], Store,
+                        "Destination address for the UDP data (formatted host_ip:port_number)");
+        parser.refer(&mut port)
+            .add_option(&["-p", "--port"], Store,
+                        "Port from which we send the data (this program binds this port)");
+        parser.parse_args_or_exit();
+    }
+
+    let mode = check_mode(ascii_hex, binary);
+
+    let destination = check_dest_address(dest);
+
+    let sock = check_source_socket(port);
 
     let options = ExncOptions {
         mode,
-        dest: destination
+        dest: destination,
+        sock
     };
 
     println!("Configured options: {:?}", options);
@@ -101,11 +130,6 @@ fn main() {
     let data = line_to_buffer(OperatingMode::InputIsBinary, "test_buffer".to_string()).unwrap();
     println!("{:?}", data);
 
-
-    let socket = UdpSocket::bind("127.0.0.1:45354").expect("unable to bind to UDP socket for sending data");
-
-    send_data(socket, data, options.dest);
-
-
+    send_data(options.sock, data, options.dest);
 
 }

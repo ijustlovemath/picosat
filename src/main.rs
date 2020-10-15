3464143a9eb524e9498860a39c1c5b5c2711e24c
Jeremy <jcdejournett@gmail.com>
2020-10-15 10:25:37 -0400

    connecting the test pieces together, everything compiles

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,14 @@ use std::vec::Vec;
 // --ascii: expect input in newline-terminated hexadecimal strings, 0-9a-fA-F
 // --binary: direct binary stream as expected (until eof) to network port
 
-fn send_data<SocketType>(socket: UdpSocket, data: Vec::<u8>, destination_address: SocketAddr) -> std::io::Result<()> {
-    {
-        socket.send_to(&data, &destination_address);
-    }
-    Ok(()) // what does this do?
+fn send_data(socket: UdpSocket, data: Vec::<u8>, destination_address: SocketAddr) -> std::io::Result<()> {
+    let result = socket.send_to(&data, &destination_address);
+
+    let result = match result {
+        Ok(bytes) => Ok(()),
+        Err(error) => panic!("Unable to send data to {:?} because {:?}", destination_address, error),
+    };
+    result
 }
 
 enum OperatingMode {
@@ -20,23 +23,30 @@ enum OperatingMode {
     InputIsBinary
 }
 
-fn line_to_buffer(mode: OperatingMode, /*mut result: &Vec::<u8>,*/ line: String) 
+fn line_to_buffer(mode: OperatingMode, line: String) 
     -> std::result::Result<Vec::<u8>, &'static str> {
     match mode {
         OperatingMode::InputIsHex => {
             let result = Vec::<u8>::new();
             Ok(result)
         },
+        OperatingMode::InputIsBinary => {
             //result = &Vec::<u8>::from(line.as_bytes());
             let result = Vec::<u8>::from(line);
             Ok(result)
         },
-        _ => Err("shits fucked man")
+        _ => Err("Unrecognized mode")
     }
 }
 
 fn main() {
-    println!("Hello, world!");
+    let data = line_to_buffer(OperatingMode::InputIsBinary, "test_buffer".to_string()).unwrap();
+    println!("{:?}", data);
 
-    println!("{:?}", line_to_buffer(OperatingMode::InputIsBinary, "test buffer".to_string()).unwrap());
+    let dest: SocketAddr = "127.0.0.1:8080".parse().unwrap();
+
+    let socket = UdpSocket::bind("127.0.0.1:45354").expect("unable to bind to UDP socket for sending data");
+
+    send_data(socket, data, dest);
+
 }
